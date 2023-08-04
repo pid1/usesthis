3c32417c6a8f83d4dce5dcefa682962ae4c6142f
Jonathan Roemer <jonathan@incrediblehq.com>
2023-08-04 09:56:11 -0500

    Add usage instructions

--- shell.nix
+++ shell.nix
@@ -1,3 +1,5 @@
+# After installing nix, invoke nix-shell in the directory containing this file
+
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {} }:
 
 pkgs.mkShell {
