713f0d814c811b5d1dc4d136d734f376c762c77f
Jonathan Roemer <win@cygnusstrategy.com>
2025-05-11 10:18:06 -0500

    Clean up unused configs

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-export DEBIAN_FRONTEND=noninteractive
-
 # Don't be noisy on login
 touch /root/.hushlogin
-
-# Install what we need
-apt update
-apt install -y build-essential \
-            httpie \
-            manpages-posix-dev \
-            mosh \
-            neovim \
-            ripgrep \
-            tmux \
-            unzip \
-            valgrind
 
 # Set up host keys without user intervention
 ssh-keygen -q -t ed25519 -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
@@ -24,10 +10,3 @@ ssh-keygen -q -t ed25519 -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
 echo 'SHELL=/bin/sh \
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin \
 */1 * * * *   root curl https://github.com/pid1.keys > /root/.ssh/authorized_keys' > /etc/cron.d/keys
-
-# Configs
-curl https://raw.githubusercontent.com/pid1/usesthis/refs/heads/main/dotfiles/tmux.conf > /root/.tmux.conf
-curl https://raw.githubusercontent.com/pid1/usesthis/refs/heads/main/dotfiles/bash_aliases > /root/.bash_aliases
-
-mkdir -p /root/.config/nvim
-curl https://raw.githubusercontent.com/pid1/usesthis/refs/heads/main/dotfiles/init.vim > /root/.config/nvim/init.vim
