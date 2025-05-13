426775653e31f3576019073df8afa41525cbcc41
Jonathan Roemer <dev@roemersoftworks.com>
2025-05-12 19:00:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ ssh-keygen -q -t ed25519 -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
 # SSH key management
 echo 'SHELL=/bin/sh \
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin \
-*/1 * * * *   root curl https://github.com/pid1.keys > /root/.ssh/authorized_keys' > /etc/cron.d/keys
+*/1 * * * *   root curl https://github.com/pid1.keys > /root/.ssh/authorized_keys' > /etc/cron.d/keys'
